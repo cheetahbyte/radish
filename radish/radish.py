d2ac4bf4eefa9ebd68fb28933bc7bf1c580d3304
cheetahbyte <bernerdoodle@outlook.de>
2022-02-16 17:18:19 +0100

    feat(radix): radixtree implementation

--- radish/radish.py
+++ radish/radish.py
@@ -1,23 +1,27 @@
 from typing import List, Mapping, Callable
-from radish.pool import Pool
+from radish.pool import Node
 
 class Radish:
     """Radish Router"""
     methods: List[str] = ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS", "TRACE"]
 
     def __init__(self, trim_trailing_slash: bool = True) -> None:
-        self.pools: Mapping[str, Pool] = {}
+        self.pools: Mapping[str, Node] = {}
         self.trim_trailing_slash: bool = trim_trailing_slash
         for method in self.methods:
-            self.pools[method.lower()] = Pool(method)
+            self.pools[method.lower()] = Node()
 
     def insert(self, method: str, route: str, handler: Callable, **kwargs: dict) -> None:
         """insert a new route into the pool"""
-        self.pools[method.lower()].insert(route, handler, **kwargs)
+        if route.endswith("/") and len(route) > 1 and self.trim_trailing_slash:
+            route = route[:-1]
+        self.pools[method.lower()].add(route, handler)
 
-    def get(self, method: str, route: str) -> Callable:
+    def get(self, method: str, route: str) -> dict:
         """get the handler for a route"""
-        return self.pools[method.lower()].get(route)
+        if route.endswith("/") and len(route) > 1 and self.trim_trailing_slash:
+            route = route[:-1]
+        return self.pools[method.lower()].find(route)
 
     def __repr__(self) -> str:
         return f"<RadishRouter pools={self.pools}>"

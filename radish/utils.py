3e31cf10fcb4605fb724f037761aa29ccba8a061
cheetahbyte <bernerdoodle@outlook.de>
2022-02-17 14:59:12 +0100

    feat(triage): add triage function to decide over routes

--- radish/utils.py
+++ radish/utils.py
@@ -1,25 +1,57 @@
-from typing import Tuple
+import re
+from sre_constants import MIN_UNTIL
+from typing import List
+from uuid import UUID
+from radish.typings import Route, StoredRoute, WildCardVar
+from pprint import pprint
+
+var_types = {
+    "int": int,
+    "float": float,
+    "str": str,
+    "uuid": UUID,
+}
+
+def matches(path: str, regex) -> bool:
+    match = re.match(regex, path, re.IGNORECASE)
+    return match is not None
 
 
-def throw(expr: str, msg: str) -> None:
-    if not expr:
-        raise Exception(msg)
 
-def is_wildcard(c: str) -> bool:
-    assert len(c) == 1, "Length of wildcard is only 1, please provide a name"
-    return c == ":" or c == "*"
+def make_route(path: str, stored_route: StoredRoute) -> dict | None:
+    route: Route = {"path": path, "handler": None, "params": {}}
+    matches = re.findall(stored_route["path"], path, re.IGNORECASE)
+
+    # if matches[0] is tuple
+    if isinstance(matches[0], tuple):
+        # convert matches to list
+        matches = list(matches[0])
+
+    for i, var in enumerate(stored_route["vars"]):
+        match = matches[i]
+        typ = var_types[var.get("var_type") ]if var.get("var_type") else str
+        route["params"]["{}".format(var["var"])] = typ(match)
+    route["handler"] = stored_route["handler"]
+    return route
 
 
-def split_from_first_slash(path: str) -> Tuple[str, str]:
-    i = 0
-    while i < len(path) and path[i] != "/":
-        i += 1
-    return (path[0:i], path[i:])
 
-def longest_common_prefix(a: str, b: str) -> int:
-    i = 0
-    length = min(len(a), len(b))
-    while i < length and a[i] == b[i]:
-        i += 1
-    return i
+def conv_wildcards(wildcard: dict) -> WildCardVar:
+    return {"var": wildcard["wildcard"][1:], "var_type": "str", "extra": "wildcard"}
 
+
+def triage(routes: List[Route]) -> Route:
+    """
+    This function will be used to determine which route to use
+    """
+    eva = {}
+    for i, route in enumerate(routes):
+        stored_route: StoredRoute = route.pop("stored_route")
+        reg_path = stored_route["path"]
+        segments = [s for s in reg_path.split("/") if s]
+        eva[i] = len([seg for seg in segments if not seg.startswith("(?P") and not seg.endswith(")")])
+    dt = {v: routes[k] for k, v in eva.items()}
+    return dt[max(dt)]
+    
+
+   

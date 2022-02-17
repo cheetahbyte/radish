25ca2b4f75c34af20e4a6423932f04587ec5fda4
cheetahbyte <bernerdoodle@outlook.de>
2022-02-17 14:59:38 +0100

    feat(router): add functionality

--- radish/radish.py
+++ radish/radish.py
@@ -1,27 +1,28 @@
 from typing import List, Mapping, Callable
-from radish.pool import Node
+from radish.pool import Pool
+from radish.exceptions import MethodNotAllowed
 
 class Radish:
     """Radish Router"""
     methods: List[str] = ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS", "TRACE"]
 
     def __init__(self, trim_trailing_slash: bool = True) -> None:
-        self.pools: Mapping[str, Node] = {}
+        self.pools: Mapping[str, Pool] = {}
         self.trim_trailing_slash: bool = trim_trailing_slash
         for method in self.methods:
-            self.pools[method.lower()] = Node()
+            self.pools[method.lower()] = Pool(method)
 
-    def insert(self, method: str, route: str, handler: Callable, **kwargs: dict) -> None:
+    def insert(self, method: str, route: str, handler: Callable) -> None:
         """insert a new route into the pool"""
-        if route.endswith("/") and len(route) > 1 and self.trim_trailing_slash:
-            route = route[:-1]
-        self.pools[method.lower()].add(route, handler)
+        if (method.upper() not in self.methods):
+            raise MethodNotAllowed(method)
+        self.pools[method.lower()].insert(route, handler)
 
-    def get(self, method: str, route: str) -> dict:
+    def get(self, method: str, route: str) -> Callable:
         """get the handler for a route"""
-        if route.endswith("/") and len(route) > 1 and self.trim_trailing_slash:
-            route = route[:-1]
-        return self.pools[method.lower()].find(route)
+        if (method.upper() not in self.methods):
+            raise MethodNotAllowed(method)
+        return self.pools[method.lower()].get(route)
 
     def __repr__(self) -> str:
         return f"<RadishRouter pools={self.pools}>"
